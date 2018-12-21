8b5a43ffd11ec842dd1f4844b27a4e01e6cf7dba
lafesh <ginafedder@yahoo.com>
2018-12-20 17:19:49 -0700

    added reviews as a resource on its on. started creating new game form

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
     resources :reviews
   end
 
+  resources :reviews
+
   
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

9800ca3880bdae304002690b71c690b56f683f50
lafesh <ginafedder@yahoo.com>
2018-12-23 14:09:01 -0700

    created update and destroy method for games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,6 +5,16 @@ class GamesController < ApplicationController
         @game = Game.new
     end
 
+    def create
+        game = Game.new(game_params)
+        if game.save
+            redirect_to new_game_review_path(game.id)
+        else 
+            flash[:alert] = "Please fill out all the fields"
+            redirect_to new_game_path 
+        end
+    end
+
     def select_game 
         @games = Game.all   
     end 
@@ -25,14 +35,26 @@ class GamesController < ApplicationController
         @game = Game.find(params[:id])
     end
 
-    def create
-        game = Game.new(game_params)
+    def edit
+        @game = Game.find(params[:id])
+    end
+
+    def update
+        game = Game.find(params[:id])
+        game.update(game_params)
         if game.save
-            redirect_to new_game_review_path(game.id)
-        else 
-            flash[:alert] = "Please fill out all the fields"
-            redirect_to new_game_path 
+            redirect_to game_path(game)
+        else
+            flash[:alert] = "Please fill out all the fields!"
+            redirect_to edit_game_path(game)
         end
+    end
+
+    def destroy
+        game = Game.find(params[:id])
+        flash[:alert] = "Your game #{game.name} has successfully been deleted!"
+        game.delete
+        redirect_to games_game_overview_path
     end
 
     private

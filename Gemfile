dbbcca333135eddfa4c6234e41f350ea5984b403
lafesh <ginafedder@yahoo.com>
2019-01-06 17:33:24 -0700

    implemented facebook omniauth, has error though

--- Gemfile
+++ Gemfile
@@ -63,3 +63,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'devise'
 gem 'pry'
+gem 'dotenv-rails'
+gem 'omniauth-facebook'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_20_220043) do
+ActiveRecord::Schema.define(version: 2019_01_07_000128) do
 
   create_table "games", force: :cascade do |t|
     t.string "name"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 2018_12_20_220043) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
